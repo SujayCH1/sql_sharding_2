cc9b13bf245e8f9f732b7e7a8d116f126b9cf9f8
Sujay Chanageri <suju.narayan@gmail.com>
2026-01-26 13:33:05 +0000

    modified migrate file to allow app build

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,22 +1,37 @@
 package database
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func RunMigrations(dsn string) error {
+	exePath, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	exeDir := filepath.Dir(exePath)
+
+	projectRoot := filepath.Clean(filepath.Join(exeDir, "..", ".."))
+
+	migrationsPath := filepath.Join(projectRoot, "migrations")
+
+	sourceURL := "file://" + migrationsPath
+
 	m, err := migrate.New(
-		"file://migrations",
+		sourceURL,
 		dsn,
 	)
 	if err != nil {
 		return err
 	}
 
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		return err
 	}
 
